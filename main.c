f29200174c77333b865de91fee705e69d449367c
tismajo <gir23559@uvg.edu.gt>
2024-10-20 17:42:45 -0600

    main

--- main.c
+++ main.c
@@ -1,34 +1,59 @@
-#include "FUNCS.h"
+#include "FUNCS.h" // Incluye las declaraciones
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-// Primer laberinto creado.
-char maze[11][31] = {
-    "##############################",
-    "#............##............###",
-    "#.####.#####.##.#####.####..##",
-    "#.#  #.#   #.##.#   #.#  #..##",
-    "#.#  #.#   #.##.#   #.#  #..##",
-    "#.####.#####.##.#####.####..##",
-    "#..........................###",
-    "#.####.##.########.##.####..##",
-    "#.####.##.########.##.####..##",
-    "#............##............###",
-    "##############################"
-};
-
-pthread_mutex_t mazeLock = PTHREAD_MUTEX_INITIALIZER;
-
-// Función para mostrar el laberinto.
-void printMaze(PacMan *pacman) {
-    pthread_mutex_lock(&mazeLock);
-
-    for (int i = 0; i < 11; i++) {
-        printf("%s\n", maze[i]);
+void* pacmanThread(void* arg) {
+    PacMan *pacman = (PacMan*) arg;
+    char input;
+    
+    while (pacman->lives > 0) {
+        printf("Ingrese la dirección de movimiento (w: arriba, s: abajo, a: izquierda, d: derecha): ");
+        scanf(" %c", &input);
+        
+        pacManMovement(pacman, input);
+        printf("\n");
+        printMaze(pacman);
     }
 
-    if (pacman != NULL) {
-        printf("Score: %d\n", pacman->score);
+    return NULL;
+}
+
+void* ghostThread(void* arg) {
+    Ghost *ghost = (Ghost*) arg;
+    while (ghost->lives > 0) {
+        ghostMovement(ghost);
+        printf("\n");
+        printMaze(NULL);
+        sleep(1);
     }
 
-    pthread_mutex_unlock(&mazeLock);
+    return NULL;
+}
+
+int main() {
+    srand(time(NULL));  // Inicializar srand() solo una vez.
+
+    PacMan pacman;
+    Ghost ghost1;
+    
+    initPacMan(&pacman, 1, 1); // Inicializamos a Pac-Man en una posición válida
+    initGhost(&ghost1, 1, 25);
+
+    printMaze(&pacman);
+
+    pthread_t pacman_tid, ghost_tid;
+
+    pthread_create(&pacman_tid, NULL, pacmanThread, &pacman);
+    pthread_create(&ghost_tid, NULL, ghostThread, &ghost1);
+
+    pthread_join(pacman_tid, NULL);
+    pthread_join(ghost_tid, NULL);
+
+    pthread_mutex_destroy(&mazeLock);  // Destruir mutex principal
+    pthread_mutex_destroy(&pacman.lock);  // Destruir mutex de PacMan
+    pthread_mutex_destroy(&ghost1.lock);  // Destruir mutex del fantasma
+
+    return 0;
 }
